ee0506bae3bf8bc140e01ada7c79119dd0ab70d3
ravikirany711 <ravikirany711@gmail.com>
2019-05-12 13:40:23 +0530

    Weather component added

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-class Form extends React.Component {
+class Weather extends React.Component {
     render() {
         return (
             <div>
-                <h1>The  Form Component</h1>
+                <h1>The  Weather Component</h1>
 
             </div>
         )
     }
 }
 
-export default Form 
+export default Weather
