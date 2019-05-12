0e2a3199533285811a15c47ea643799e663ab4ce
ravikirany711 <ravikirany711@gmail.com>
2019-05-12 17:26:31 +0530

    state introduced and props extracted

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ const API_KEY = "70a77469dbb4c38512c8c43ae0fb4fa9"
 
 
 class App extends React.Component {
+  state = {
+    temparature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+
+  }
   getWeather = async (e) => {
     e.preventDefault()
     const city = e.target.elements.city.value //whatever we type in to the input
@@ -16,6 +25,15 @@ class App extends React.Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
     const data = await api_call.json()
     console.log(data)
+
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    })
   }
   render() {
     return (
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,7 @@ class Weather extends React.Component {
     render() {
         return (
             <div>
-                <h1>The  Weather Component</h1>
+
 
             </div>
         )
